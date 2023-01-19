79b5119653e5d473a7a4e3d03558f6c9407da500
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2023-01-19 14:19:06 +0200

    add feedback & asvantages

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector(".header-open-btn"),
     closeMenuBtn: document.querySelector(".mob-close-btn"),
     menu: document.querySelector(".mob"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("hidden");
   }
 })();
